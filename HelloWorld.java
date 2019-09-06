a5b02ef0142072c3cd26cb9586adcc4f433b89fe
Christian Mandapat <cmandapat1215@gmail.com>
2019-09-06 13:44:24 -0400

    Uploading to test branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(add());
+    }
+    public int add() {
+        return 1 + 2;
     }
 }
